b7ac3f3bb88dc2ca1d7027c58756ce038d377a08
nickmaher84 <nickmaher84@gmail.com>
2016-01-20 12:24:16 +0000

    Make event code into functions, not one single script

--- events.py
+++ events.py
@@ -1,21 +1,36 @@
 from settings import matches, events, players, teams
+from datetime import datetime
 
-for match in matches.find({'events': {'$exists': True}}).sort('matchId'):
-    print(match['matchId'], '{0} events'.format(len(match['events'])))
 
-    count = events.count({'matchId': match['matchId']})
-    if count and len(match['events']) == count:
-        print('Events already written')
-    else:
-        events.remove({'matchId': match['matchId']})
-        for event in match['events']:
-            for k in ['matchId', 'stageId', 'seasonId', 'tournamentId', 'regionId']:
-                if k in match:
-                    event[k] = match[k]
-            events.insert_one(event)
+def load_events():
+    print(datetime.now(), 'Starting')
+    for match in matches.find({'events': {'$exists': True}}).sort('matchId', -1):
 
-    for k, v in match['playerIdNameDictionary'].items():
-        players.update_one({'playerId': int(k)}, {'$set': {'playerName': v}}, upsert=True)
+        count = events.count({'matchId': match['matchId']})
+        if count and len(match['events']) == count:
+            # print('Events already written')
+            continue
+        else:
+            print(datetime.now(), match['matchId'], '{0} events'.format(len(match['events'])))
+            events.remove({'matchId': match['matchId']})
+            for event in match['events']:
+                for k in ['matchId', 'stageId', 'seasonId', 'tournamentId', 'regionId']:
+                    if k in match:
+                        event[k] = match[k]
+                events.insert_one(event)
+    print(datetime.now(), 'Complete')
 
-    teams.update_one({'teamId': match['home']['teamId']}, {'$set': {'teamName': match['home']['name']}}, upsert=True)
-    teams.update_one({'teamId': match['away']['teamId']}, {'$set': {'teamName': match['away']['name']}}, upsert=True)
+
+def load_players_and_teams():
+    for match in matches.find({'error': {'$exists': False}}).sort('matchId', -1):
+        print(match['matchId'])
+        if 'playerIdNameDictionary' in match:
+            for k, v in match['playerIdNameDictionary'].items():
+                players.update_one({'playerId': int(k)}, {'$set': {'name': v}}, upsert=True)
+
+        teams.update_one({'teamId': match['home']['teamId']}, {'$set': {'name': match['home']['name']}}, upsert=True)
+        teams.update_one({'teamId': match['away']['teamId']}, {'$set': {'name': match['away']['name']}}, upsert=True)
+
+if __name__ == "__main__":
+    # load_events()
+    load_players_and_teams()
